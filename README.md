27db6561fddbfc9b08891c159a923a1fb0045aea
Daniele Baieri <daniele.baieri@gmail.com>
2023-07-19 17:04:29 +0200

    begin implementing example main

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # FluidSimulationsDataset
+
+
+# Instructions
+
+1. Clone the repo
+2. `git submodule init`
+3. `git submodule update --recursive --remote`
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "fx3d/lbm.hpp"
+#include "nlohmann/json.hpp"
+
+
+
+int main() {
+    std::cout << "Hello" << std::endl;
+
+    
+}
